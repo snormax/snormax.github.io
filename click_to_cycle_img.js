98526ec542eafd2b4e87b6a778a9dd3857ca577f
snormax <53067950+snormax@users.noreply.github.com>
2020-12-17 01:25:33 -0800

    fix js counter and order

--- click_to_cycle_img.js
+++ click_to_cycle_img.js
@@ -1,17 +1,17 @@
-let counter = 0;
+let counter = 1;
 
 function click_to_cycle_img() {
     switch (counter) {
         case 0:
-            document.getElementById("reddit_bot_img").src = "img/bot_dm.png";
+            document.getElementById("reddit_bot_img").src = "img/bot_winner.png";
             counter++;
             break;
         case 1:
-            document.getElementById("reddit_bot_img").src = "img/bot_main.png";
+            document.getElementById("reddit_bot_img").src = "img/bot_dm.png";
             counter++;
             break;
         case 2:
-            document.getElementById("reddit_bot_img").src = "img/bot_winner.png";
+            document.getElementById("reddit_bot_img").src = "img/bot_main.png";
             counter++;
             break;
         case 3:
