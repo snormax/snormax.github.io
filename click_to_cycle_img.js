8e0c68242a010319a09938f30e80f6dc0bc77714
snormax <53067950+snormax@users.noreply.github.com>
2021-01-20 20:12:38 -0800

    add Beat Sort project and accept multiline nav

--- click_to_cycle_img.js
+++ click_to_cycle_img.js
@@ -1,52 +1,44 @@
-function set_buffers(){
-    let header_height = document.getElementById("header").offsetHeight;
-    if (header_height > 0) {
-        document.getElementById("article").style.paddingTop = String(header_height * .7) + "px";
-    }
-}
-
-
 let reddit_counter = 0;
 
-function right_cycle_reddit_bot_img() {
+function reddit_right() {
     if (reddit_counter === 3) {
         reddit_counter = 0;
     } else {
         reddit_counter++;
     }
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
-function left_cycle_reddit_bot_img() {
+function reddit_left() {
     if (reddit_counter === 0) {
         reddit_counter = 3;
     } else {
         reddit_counter--;
     }
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
 function reddit_dot_0() {
     reddit_counter = 0;
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
 function reddit_dot_1() {
     reddit_counter = 1;
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
 function reddit_dot_2() {
     reddit_counter = 2;
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
 function reddit_dot_3() {
     reddit_counter = 3;
-    check_reddit_counter_switch();
+    reddit_check_counter_switch();
 }
 
-function check_reddit_counter_switch() {
+function reddit_check_counter_switch() {
     switch (reddit_counter) {
         case 0:
             document.getElementById("reddit_bot_img").src = "img/bot_winner.png";
